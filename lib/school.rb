e22839cbca81e853785a7d74a27e77bc187ad5c1
Venice Vargas <venicevargas1@live.com>
2020-09-08 04:05:04 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -20,5 +20,30 @@ class School
       end 
     end 
   end 
-  
 end
+
+def sort 
+  nu_hash = {}
+  roster.each do |x, y| 
+    nu_hash[x] = y.sort 
+  end 
+  nu_hash
+end 
+  
+end 
+
+
+
+school = School.new("Bayside High School")
+school.roster
+school.add_student("Zach Morris", 9)
+school.add_student("Moshe Zeus", 9)
+school.add_student("Onkelos Octavius", 9)
+school.roster
+school.add_student("Sugar Zaza", 5)
+school.roster
+school.add_student("Theo Madus", 5)
+school.add_student("Abbah Zabbah", 2)
+school.roster
+school.grade(9)
+school.sort
