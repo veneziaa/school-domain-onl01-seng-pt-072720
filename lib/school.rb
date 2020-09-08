e1223de413ccabd82b4a4ad2ef13f22d1f52bca8
Venice Vargas <venicevargas1@live.com>
2020-09-08 03:54:02 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,4 +1,11 @@
 school = School.new("Bayside High School")
 def school.roster
-school.add_student("Zach Morris",9)
+
+school.roster
+
+
+
+school.add_student("AC Slater", 9)
+school.add_student("Kelly Kapowski", 10)
+school.add_student("Screech", 11)
 school.roster
